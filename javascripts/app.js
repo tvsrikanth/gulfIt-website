73316305de83a50c29550835f7aade646497322c
Srikanth.Venkatesh <Srikanth.Venkatesh@zmail.ril.com>
2019-04-26 00:47:28 +0530

    style changes

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,6 +1,6 @@
    $(document).ready(function(){
 	$('.sleekslider').sleekslider({
-		thumbs: ['slide61.jpg', 'slider2.jpg', 'slider3.jpg','slider4.jpg','slider5.jpg'],
+		thumbs: ['slider1.jpg', 'slider2.jpg', 'slider3.jpg','slider4.jpg','slider5.jpg'],
 		labels:['Slide 1', 'Slide 2', 'Slide 3', 'Slide 4', 'Slide 5'],
 		speed: 6000
 	});
