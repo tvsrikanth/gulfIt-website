db686675a97bfb7a99b0f47bdf2b0d500143871b
Srikanth.Venkatesh <Srikanth.Venkatesh@zmail.ril.com>
2019-04-27 23:52:57 +0530

    navigation changes

--- js/main.js
+++ js/main.js
@@ -1,74 +1,7 @@
-$(document).ready(function () {
-
-    // Instantiate the Bootstrap carousel
-    $('.multi-item-carousel').carousel({
-        interval: false
-    });
-
-    // for every slide in carousel, copy the next slide's item in the slide.
-    // Do the same for the next, next item.
-    $('.multi-item-carousel .item').each(function () {
-        var next = $(this).next();
-        if (!next.length) {
-            next = $(this).siblings(':first');
-        }
-        next.children(':first-child').clone().appendTo($(this));
-
-        for (var i = 0; i < 2; i++) {
-            next = next.next();
-            if (!next.length) {
-                next = $(this).siblings(':first');
-            }
-
-            next.children(':first-child').clone().appendTo($(this));
-        }
-    });
-
-    $(".center").slick({
-        dots: true,
-        infinite: true,
-        centerMode: true,
-        slidesToShow: 3,
-        slidesToScroll: 2,
-        responsive: [
-            {
-                breakpoint: 1200,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 1008,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 720,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 360,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-
-        ]
-    });
-
-    // $(window).resize(function () {
-    //     $('.js-slider').not('.slick-initialized').slick('resize');
-    // });
-
-    // $(window).on('orientationchange', function () {
-    //     $('.js-slider').not('.slick-initialized').slick('resize');
-    // });
+$(function() {
+    $.get('pages/header-nav.html', function(data) {
+        console.log('data'+data);
+        $('#navigation').html(data);
+    }); 
 });
 
