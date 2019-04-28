8a23199d33081ad20520eae1ce5e3ff08a7dec73
Srikanth.Venkatesh <Srikanth.Venkatesh@zmail.ril.com>
2019-04-28 16:21:20 +0530

    product list carosuel

--- javascripts/app.js
+++
@@ -1,7 +0,0 @@
-   $(document).ready(function(){
-	$('.sleekslider').sleekslider({
-		thumbs: ['slider1.jpg', 'slider2.jpg', 'slider3.jpg','slider4.jpg','slider5.jpg'],
-		labels:['Slide 1', 'Slide 2', 'Slide 3', 'Slide 4', 'Slide 5'],
-		speed: 6000
-	});
-});
